383281f4beca1c4113b5bffdf9404eeb8dd8afb6
himarajab <rajabhima2@gmail.com>
2020-10-29 09:02:52 +0200

    fixing pytest issues

--- bin/activate.fish
+++ bin/activate.fish
@@ -57,7 +57,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/hima/Downloads/Video/vscode/n/pytest django and drf/venv'
+set -gx VIRTUAL_ENV '/home/hima/Downloads/Video/vscode/n/pytest_django_and_drf/venv'
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | head -c 1) -lt 3
--- classroom/tests.py
+++ classroom/tests.py
@@ -1,75 +1,52 @@
 from .models import Student
 from django.test import TestCase
-
+from mixer.backend.django import mixer
 # Create your tests here.
 
 class TestStudentmodel(TestCase):
-  def setUp(self):
-    self.student1 = Student.objects.create(
-      first_name = "Tom",
-      last_name = "hima",
-      admission_number=12345,
+  # def setUp(self):
+  #   self.student1 = Student.objects.create(
+  #     first_name = "Tom",
+  #     last_name = "hima",
+  #     admission_number=1234,
 
-    )
+  #   )
   
   def test_student_can_created(self):
-    # student1 = Student.objects.create(
-    #   first_name = "Tom",
-    #   last_name = "hima",
-    #   admission_number=1234,
+    
+    student1 = mixer.blend(Student,first_name='Tom')
+    student_result = Student.objects.last()
 
-    # )
-
-    # student_result = Student.objects.last()
-
-    self.assertEqual(self.student1.first_name ,'Tom')
+    self.assertEqual(student_result.first_name ,'Tom')
 
   def test_str_resturn_self(self):
-    # student1 = Student.objects.create(
-    #   first_name = "Tom",
-    #   last_name = "hima",
-    #   admission_number=1234,
+   
+    student1 = mixer.blend(Student,first_name='Tom')
 
-    # )
 
-    # student_result = Student.objects.last()
+    student_result = Student.objects.last()
 
-    self.assertEqual(str(self.student1) ,'Tom')
+    self.assertEqual(str(student_result) ,'Tom')
 
   def test_grade_fail(self):
-    # student1 = Student.objects.create(
-    #   first_name = "Tom",
-    #   last_name = "hima",
-    #   admission_number=1234,
-    #   average_score=10
-    # )
+   
+    student1 = mixer.blend(Student,average_score=10)
 
-    # student_result = Student.objects.last()
+    student_result = Student.objects.last()
 
-    self.assertEqual(self.student1.get_grade() ,"Fail")
+    self.assertEqual(student_result.get_grade() ,"Fail")
 
 
   def test_grade_pass(self):
-      # student1 = Student.objects.create(
-      #   first_name = "Tom",
-      #   last_name = "hima",
-      #   admission_number=1234,
-      #   average_score=60
-      # )
+    student1 = mixer.blend(Student,average_score=60)
+    student_result = Student.objects.last()
 
-      # student_result = Student.objects.last()
-
-      self.assertEqual(self.student1.get_grade() ,"Pass")
+    self.assertEqual(student_result.get_grade() ,"Pass")
 
 
   def test_grade_excellent(self):
-      # student1 = Student.objects.create(
-      #   first_name = "Tom",
-      #   last_name = "hima",
-      #   admission_number=1234,
-      #   average_score=90
-      # )
+    student1 = mixer.blend(Student,average_score=90)
 
-      # student_result = Student.objects.last()
+    student_result = Student.objects.last()
 
-      self.assertEqual(self.student1.get_grade() ,"Excellent")
+    self.assertEqual(student_result.get_grade() ,"Excellent")
